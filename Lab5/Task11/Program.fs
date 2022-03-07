7691190bb9a1d50d7e06ec3dad86f2053b9efa38
r1411 <roma00055@gmail.com>
2022-03-07 16:38:23 +0300

    Add project files.

---
+++ Lab5/Task11/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+let answer_to = function
+    | "f#" | "prolog" -> "Пoдлизa!"
+    | "python" -> "На змеином не разгорвариваю"
+    | "java" -> "Приглашаю на чашечку Java"
+    | "c#" -> "Мистер Билл Гейтс передает вам привет"
+    | "c++" -> "Отлично"
+    | "c" -> "Здорово"
+    | "php" -> "Купи слона"
+    | _ -> "Такого не знаю"
+
+[<EntryPoint>]
+let main argv =
+    let lang = Console.ReadLine().ToLower()
+    Console.WriteLine(answer_to lang)
+    0
