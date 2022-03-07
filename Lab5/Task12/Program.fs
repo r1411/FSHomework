dc20fb23a2ebd10006659262821690ebe8614026
r1411 <roma00055@gmail.com>
2022-03-07 17:26:10 +0300

    Задача 5.12

---
+++ Lab5/Task12/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let lower (x: String) = x.ToLower()
+    let answer_to x =
+        match x with
+        | "f#" | "prolog" -> "Пoдлизa!"
+        | "python" -> "На змеином не разгорвариваю"
+        | "java" -> "Приглашаю на чашечку Java"
+        | "c#" -> "Мистер Билл Гейтс передает вам привет"
+        | "c++" -> "Отлично"
+        | "c" -> "Здорово"
+        | "php" -> "Купи слона"
+        | _ -> "Такого не знаю"
+
+    
+    //12.1
+    printfn "Какoй твoй любимый язык?"
+    (Console.ReadLine >> lower >> answer_to >> Console.WriteLine)()
+
+    //12.2
+    printfn "Какoй твoй любимый язык?"
+    let proc input (output:string->unit) chooser = output (chooser (input ()))
+    proc Console.ReadLine Console.WriteLine answer_to
+
+    0
