a3afbf909f3453be7ac67d5fc7cff52a80d6deae
r1411 <roma00055@gmail.com>
2022-03-08 14:05:31 +0300

    Задача 5.18, Метод 2

--- Lab5/Task18/Program.fs
+++ Lab5/Task18/Program.fs
@@ -23,12 +23,22 @@ let sum_not_prime_dividers x =
             let new_divider = current_divider - 1
             sum_not_prime_dividers_r x new_sum new_divider
     sum_not_prime_dividers_r x 0 x
-    
+
+let sub_3_dig_count x =
+    let rec sub_3_dig_cnt_r x cnt =
+        if x = 0 then cnt
+        else
+            let new_cnt = if x % 10 < 3 then cnt + 1 else cnt
+            let new_x = x / 10
+            sub_3_dig_cnt_r new_x new_cnt
+    sub_3_dig_cnt_r x 0
+
 [<EntryPoint>]
 let main argv =
     Console.WriteLine("Введите число")
     let x = Console.ReadLine() |> Int32.Parse
 
-    printf "Сумма непростых делителей: %d" (sum_not_prime_dividers x)
+    printfn "Сумма непростых делителей: %d" (sum_not_prime_dividers x)
+    printfn "Кол-во цифр, меньше 3: %d" (sub_3_dig_count x)
 
     0
