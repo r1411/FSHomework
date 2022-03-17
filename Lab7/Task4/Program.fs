c0bfe4318d0e1d1919151867d20c393c704be33c
r1411 <roma00055@gmail.com>
2022-03-17 23:44:38 +0300

    Задача 7.4 (1.36)

---
+++ Lab7/Task4/Program.fs
@@ -0,0 +1,33 @@
+﻿open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+        let Head = Console.ReadLine() |> Convert.ToInt32
+        let Tail = readList (n-1)
+        Head::Tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        writeList tail
+
+[<EntryPoint>]
+let main argv =
+    printfn "Кол-во элементов:"
+    let cnt = Console.ReadLine() |> Convert.ToInt32
+    printfn "Список:"
+    let list = readList cnt
+    
+    let sorted = List.sort list
+    let rev_sorted = List.rev sorted
+
+    let max_odd = List.tryFind (fun x -> x % 2 = 1) rev_sorted
+
+    if Option.isNone max_odd then
+        printfn "В списке нет нечетных элементов"
+    else
+        printfn "Максимальный нечетный элемент: %d" (Option.get max_odd)
+
+    0
