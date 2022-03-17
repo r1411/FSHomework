b3450297d31e9ddd1749ca5dd95b8593e9df9bc6
r1411 <roma00055@gmail.com>
2022-03-17 23:10:21 +0300

    Фикс 7.2 (1.16)

--- Lab7/Task2/Program.fs
+++ Lab7/Task2/Program.fs
@@ -20,19 +20,18 @@ let main argv =
     printfn "Список:"
     let list = readList cnt
     
-    let max1 = List.max list
-    let max2 = List.reduce (fun accum x -> if x > accum && x <> max1 then x else accum) list
+    let max_elem = List.max list
     
-    let max1_index = List.findIndex (fun x -> x = max1) list
-    let max2_index = List.findIndex (fun x -> x = max2) list
-
-    let from_index = min max1_index max2_index
-    let to_index = max max1_index max2_index
-
-    let between_indexed = List.filter (fun x -> fst x > from_index && fst x < to_index) (List.indexed list)
-    let between = List.map (fun x -> snd x) between_indexed
-
-    printfn "Элементы между двумя максимальными:"
-    writeList between
+    let max1_index = List.findIndex (fun (idx, x) -> x = max_elem) (List.indexed list)
+    let max2_index = List.tryFindIndex (fun (idx, x) -> idx <> max1_index && x = max_elem) (List.indexed list)
     
+    if max2_index = None then
+        printfn "В списке нет второго максимального числа"
+    else
+        let between_indexed = List.filter (fun x -> fst x > max1_index && fst x < Option.get max2_index) (List.indexed list)
+        let between = List.map (fun x -> snd x) between_indexed
+
+        printfn "Элементы между первым и вторым максимальными:"
+        writeList between
+
     0
