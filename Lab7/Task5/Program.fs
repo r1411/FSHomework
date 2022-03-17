36066e5e489a2f14eebe725194bdb7cd7ebc1f5f
r1411 <roma00055@gmail.com>
2022-03-17 23:49:27 +0300

    Задача 7.5 (1.46)

---
+++ Lab7/Task5/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+        let Head = Console.ReadLine() |> Convert.ToInt32
+        let Tail = readList (n-1)
+        Head::Tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        writeList tail
+
+[<EntryPoint>]
+let main argv =
+    printfn "Кол-во элементов:"
+    let cnt = Console.ReadLine() |> Convert.ToInt32
+    printfn "Список:"
+    let list = readList cnt
+    
+    printfn "Все положительные элементы:"
+    List.filter (fun x -> x >= 0) list |> writeList
+
+    printfn "Все отрицательные элементы:"
+    List.filter (fun x -> x < 0) list |> writeList
+
+    0
