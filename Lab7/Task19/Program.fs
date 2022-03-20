d67334924b88ba975d377057810aad3fa874eca1
r1411 <roma00055@gmail.com>
2022-03-20 21:22:13 +0300

    Задача 7.19 (Метод 12)

--- Lab7/Task19/Program.fs
+++ Lab7/Task19/Program.fs
@@ -11,7 +11,13 @@ let shuffle_inside str =
         let transposition = [0] @ (List.sortBy(fun _ -> rnd.Next(1, len-2)) order) @ [len - 1]
         String.init len (fun idx -> str.[transposition.[idx]].ToString())
 
+// Расположить все цифры в начале строки, все буквы - в конце
+let arrange str =
+    let nums_only = String.filter (fun ch -> Char.IsDigit ch) str
+    let chars_only = String.filter (fun ch -> Char.IsLetter ch) str
+    String.concat "" [nums_only; chars_only]
+
 [<EntryPoint>]
 let main argv =
-
+    
     0
