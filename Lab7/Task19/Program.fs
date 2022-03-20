7afd0aa317d81e8d8eaf3a14536c2a0625bfbd75
r1411 <roma00055@gmail.com>
2022-03-20 21:12:26 +0300

    Задача 7.19 (Метод 6)

---
+++ Lab7/Task19/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+// Перемешать символы в слове, кроме первого и последнего
+let shuffle_inside str =
+    let len = String.length str
+    if len <= 3 then
+        str
+    else
+        let rnd = System.Random()
+        let order = [1 .. len-2]
+        let transposition = [0] @ (List.sortBy(fun _ -> rnd.Next(1, len-2)) order) @ [len - 1]
+        String.init len (fun idx -> str.[transposition.[idx]].ToString())
+
+[<EntryPoint>]
+let main argv =
+
+    0
