eb761c2a783de3dbcd86a6a405f44b8fcec684b3
r1411 <roma00055@gmail.com>
2022-03-22 22:54:52 +0300

    Причесал задание 7.2 (1.16)

--- Lab7/Task2/Program.fs
+++ Lab7/Task2/Program.fs
@@ -13,6 +13,20 @@ let rec writeList = function
         printfn "%O" head
         writeList tail
 
+let between_max list = 
+    let max_elem = List.max list
+    
+    let max1_index = List.findIndex (fun (idx, x) -> x = max_elem) (List.indexed list)
+    let max2_index = List.tryFindIndex (fun (idx, x) -> idx <> max1_index && x = max_elem) (List.indexed list)
+    
+    if max2_index = None then
+        []
+    else
+        let between_indexed = List.filter (fun x -> fst x > max1_index && fst x < Option.get max2_index) (List.indexed list)
+        List.map (fun x -> snd x) between_indexed
+
+        
+
 [<EntryPoint>]
 let main argv =
     printfn "Кол-во элементов:"
@@ -20,18 +34,8 @@ let main argv =
     printfn "Список:"
     let list = readList cnt
     
-    let max_elem = List.max list
-    
-    let max1_index = List.findIndex (fun (idx, x) -> x = max_elem) (List.indexed list)
-    let max2_index = List.tryFindIndex (fun (idx, x) -> idx <> max1_index && x = max_elem) (List.indexed list)
-    
-    if max2_index = None then
-        printfn "В списке нет второго максимального числа"
-    else
-        let between_indexed = List.filter (fun x -> fst x > max1_index && fst x < Option.get max2_index) (List.indexed list)
-        let between = List.map (fun x -> snd x) between_indexed
+    let between = between_max list
 
-        printfn "Элементы между первым и вторым максимальными:"
-        writeList between
-
+    printfn "Элементы между первым и вторым максимальными:"
+    writeList between
     0
