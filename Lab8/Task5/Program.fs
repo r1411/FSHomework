b62fae0ee54286dca95db02b8b50ec6ef06a16f3
r1411 <roma00055@gmail.com>
2022-03-25 23:29:09 +0300

    Задача 8.5 (Класс документа)

---
+++ Lab8/Task5/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+type DriversLicense(first_name: string, last_name: string, series: int, number: int, issDate: DateTime, expDate: DateTime) =
+    member this.first_name = first_name
+    member this.last_name = last_name
+    member this.series = series
+    member this.number = number
+    member this.issDate = issDate
+    member this.expDate = expDate
+
+    member this.isValid() = DateTime.Now < expDate
+
+[<EntryPoint>]
+let main argv =
+    
+    0
