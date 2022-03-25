21252a47d8d87068b42e06b8f30f8c41eaa4d418
r1411 <roma00055@gmail.com>
2022-03-25 23:57:12 +0300

    Задача 8.7 (Сравнение документов по серии, номеру)

--- Lab8/Task5/Program.fs
+++ Lab8/Task5/Program.fs
@@ -10,6 +10,20 @@ type DriversLicense(first_name: string, last_name: string, series: int, number: 
 
     member this.isValid() = DateTime.Now < expDate
 
+    interface IComparable with
+        member this.CompareTo(obj: obj): int = 
+            match obj with
+            | :? DriversLicense as lic2 -> if this.series = lic2.series then this.number.CompareTo lic2.number else this.series.CompareTo lic2.series
+            | _ -> invalidArg "obj" "Cannot compare values of different types" 
+
+    override this.GetHashCode() =
+        HashCode.Combine(series, number)
+
+    override this.Equals(obj2) =
+        match obj2 with
+        | :? DriversLicense as lic2 -> (this.series = lic2.series) && (this.number = lic2.number)
+        | _ -> false
+
     override this.ToString() = $"Права{{Имя: {this.first_name}, Фамилия: {this.last_name}, Серия: {this.series}, Номер: {this.number}, Дата выдачи: {this.issDate.ToShortDateString()}, Дата истечения: {this.expDate.ToShortDateString()}}}"
 
 [<EntryPoint>]
